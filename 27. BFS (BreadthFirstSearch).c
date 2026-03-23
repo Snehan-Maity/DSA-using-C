9f6ddd8dfa034f86539a36efcbabcef5e6056dff
Snehan Maity <snehanmaity@gmail.com>
2026-03-23 22:34:20 +0530

    Updated the file

--- 27. BFS (BreadthFirstSearch).c
+++ 27. BFS (BreadthFirstSearch).c
@@ -1,80 +1,84 @@
-//DFS
+//BFS (BreadthFirstSearch) 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #define MAX 20
-int stack[MAX];
-int top = -1;
+int front = -1;
+int rear = -1;
+int Q[MAX];
 int visited[MAX];
-int weight[MAX][MAX];
 int n;
-void push(int item) {
-    if (top == MAX - 1) {
-        printf("Stack Overflow\n");
+int weight[MAX][MAX]; 
+int isFull() {
+    return (rear + 1 == MAX);
+}
+int isEmpty() {
+    return (front == rear);
+}
+void enQueue(int item) {
+    if (isFull()) {
+        printf("Queue is Full\n");
         return;
     }
-    stack[++top] = item;
+    Q[++rear] = item;
 }
-int pop() {
-    if (top == -1) {
-        printf("Stack Underflow\n");
-        return -1;
+int deQueue() {
+    if (isEmpty()) {
+        printf("Queue is Empty\n");
+        return INT_MAX;
     }
-    return stack[top--];
+    return Q[++front];
 }
-int isStackEmpty() {
-    return top == -1;
-}
-void DFS(int v) {
-    push(v);
-    visited[v] = 1;
-
-    while (!isStackEmpty()) {
-        int u = pop();
+void BFS(int v) {
+    enQueue(v);
+    visited[v]=1;
+    while (!isEmpty()) {
+        int u=deQueue();
         printf("%c ",u+'A');
 		int w;
-        for (w=n-1;w>=0;w--) {
+        for (w=0;w<n;w++) {
             if (weight[u][w] == 1 && visited[w] == 0) {
-                push(w);
                 visited[w] = 1;
+                enQueue(w);
             }
         }
     }
 }
 void readGraph() {
-    FILE *fp = fopen("DFS_Graph.txt", "r");
+    FILE *fp = fopen("BFS_Graph.txt", "r");
     if (fp == NULL) {
         printf("\nFile open failed!!\n");
         exit(1);
     }
-    fscanf(fp,"%d",&n);
+    fscanf(fp,"%d",&n);  // Read number of vertices
 	int i,j;
-    for(i=0;i<n;i++) {
-        visited[i] = 0;
-        for (j=0;j<n;j++) {
-            fscanf(fp, "%d", &weight[i][j]);
+    for (i=0;i<n;i++) {
+        visited[i]=0;
+        for(j=0;j<n;j++) {
+            fscanf(fp,"%d",&weight[i][j]);
         }
     }
     fclose(fp);
 }
 void displayGraph() {
+	int i,j;
     printf("\nAdjacency Matrix:\n");
-    int i,j;
     for(i=0;i<n;i++) {
         for(j=0;j<n;j++) {
-            printf("%2d ",weight[i][j]);
+            printf("%2d ", weight[i][j]);
         }
         printf("\n");
     }
     printf("\n");
 }
+
 int main() {
     readGraph();
     displayGraph();
     int start;
     printf("Enter starting node index (0 to %d): ", n - 1);
     scanf("%d", &start);
-    printf("DFS Traversal: ");
-    DFS(start);
+    printf("BFS Traversal: ");
+    BFS(start);
     return 0;
 }
-
